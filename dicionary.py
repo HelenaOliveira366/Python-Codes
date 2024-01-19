9f2418a00cb9e1356d65acc982ccefce9ec29f1b
Helena Oliveira <82525471+HelenaOliveira366@users.noreply.github.com>
2024-01-19 08:42:25 -0300

    Add input data by user

--- dicionary.py
+++ dicionary.py
@@ -1,15 +1,21 @@
 #Dicionário: Crie um dicionário para armazenar o nome e a nota de 3 alunos, fazendo a leitura dos valores por meio de uma estrutura de repetição. Depois, crie uma nova estrutura de repetição para somar todas as notas e retornar a média
 
 #DECLARAÇÃO DO DICIONÁRIO
-studentsGrade = {'Fulano': 80.0, 'Ciclano': 50.0, 'Deltrano': 70.0}
+studentsGrade = {}
 average = 0.0
 
+#LOOP PARA PREECHER O DICIONÁRIO COM NOMES E NOTAS (ENTRADA DE DADOS)
+for i in range(3):
+   name = input("Digite o nome do aluno: ")
+   grade = float(input("Digite a nota do aluno: "))
+   studentsGrade[name] = grade
+
 #LOOP PARA EXIBIR OS DADOS DO DICIONÁRIO
-print("NOME e NOTAS:")
+print("NOME e NOTAS")
 for key, value in studentsGrade.items():
    print(key, value)
 
-#LOOP QUE PERCORRE OS VALORES DO DICIONÁRIO
+#LOOP QUE PERCORRE E ARMAZENA TODOS OS VALORES DO DICIONÁRIO
 for key, value in studentsGrade.items():
    average = average + value
 
